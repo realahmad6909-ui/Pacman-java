ca6875321a2768c6d6b6f322404ceb59593375c2
Ahmad <realahmad6909@gmail.com>
2026-05-13 22:14:19 +0500

    Add files via upload
    Distance formula added to move ghost smartly

--- Ghost.java
+++ Ghost.java
@@ -8,25 +8,39 @@ public class Ghost {
     }
 
     public void followPacman(int pacRow, int pacCol) {
-        int dRow = 0;
-        int dCol = 0;
+        int currentRow = position.getRow();
+        int currentCol = position.getCol();
 
+        // Possible moves: Up, Down, Left, Right
+        int[][] directions = {
+                {-1, 0}, // Up
+                {1, 0},  // Down
+                {0, -1}, // Left
+                {0, 1}   // Right
+        };
 
-        if (pacRow > position.getRow()) dRow = 1;
-        else if (pacRow < position.getRow()) dRow = -1;
+        double minDistance = Double.MAX_VALUE;
+        int bestRow = currentRow;
+        int bestCol = currentCol;
 
-        if (pacCol > position.getCol()) dCol = 1;
-        else if (pacCol < position.getCol()) dCol = -1;
+        for (int[] dir : directions) {
+            int nextRow = currentRow + dir[0];
+            int nextCol = currentCol + dir[1];
 
-        if (controller.canMove(position.getRow() + dRow, position.getCol() + dCol)) {
-            position.setPosition(position.getRow() + dRow, position.getCol() + dCol);
-        } else if (controller.canMove(position.getRow() + dRow, position.getCol())) {
-            position.setPosition(position.getRow() + dRow, position.getCol());
-        } else if (controller.canMove(position.getRow(), position.getCol() + dCol)) {
-            position.setPosition(position.getRow(), position.getCol() + dCol);
+            if (controller.canMove(nextRow, nextCol)) {
+                // Euclidean Distance formula
+                double distance = Math.sqrt(Math.pow(pacRow - nextRow, 2) + Math.pow(pacCol - nextCol, 2));
+
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    bestRow = nextRow;
+                    bestCol = nextCol;
+                }
+            }
         }
-    }
 
+        position.setPosition(bestRow, bestCol);
+    }
     public int getRow() { return position.getRow(); }
     public int getCol() { return position.getCol(); }
 }
